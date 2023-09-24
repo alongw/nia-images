19bb22029f95fbc65f80afee789081f6f9dee690
alongw <alongw@alongw.cn>
2023-09-24 21:54:13 +0800

    feat: init express server

--- service/src/app.ts
+++ service/src/app.ts
@@ -1,2 +1,10 @@
+import express from 'express'
+
 import logger from './utils/log'
-logger.info('Hello World!')
+import config from './utils/config'
+
+const app = express()
+
+app.listen(config.port, () => {
+    logger.info(`nia-images 服务器正在端口 ${config.port} 上运行`)
+})
---
+++ service/src/utils/config.ts
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import yaml from 'yaml'
+
+interface ConfigType {
+    port: number
+    db: {
+        host: string
+        port: number
+        user: string
+        password: string
+        database: string
+    }
+}
+
+const config = yaml.parse(fs.readFileSync('./config.yaml', 'utf8')) as ConfigType
+
+export default config
