8eaf3c340998de26df9d5dd2a0e38fa5b4096851
alongw <alongw@alongw.cn>
2023-09-28 16:27:43 +0800

    fix: 修复文字排版

--- service/src/utils/db.ts
+++ service/src/utils/db.ts
@@ -5,7 +5,7 @@ import logger from './log'
 const { host, user, password, database, port } = dbConfig.db
 
 logger.info(
-    `尝试使用用户 ${user} 密码 *** 连接到远程 SQL 服务器 ${host}:${port} ${database}数据库`
+    `尝试使用用户 ${user} 密码 *** 连接到远程 SQL 服务器 ${host}:${port} ${database} 数据库`
 )
 export const db = mysql.createPool({
     host,
